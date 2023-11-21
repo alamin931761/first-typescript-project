d1bc353bc0f2eef08d8bfadfce7fe3ff4ab8c440
Al Amin <alamin931761@gmail.com>
2023-11-22 03:32:54 +0600

    create student features

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  const a = 5;
+// application routes
+app.use('/api/v1/students', StudentRoutes);
 
-  res.send(a);
-});
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello World');
+};
+
+app.get('/', getAController);
 
 export default app;
